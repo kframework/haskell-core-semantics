1298c8aadf336b704153a68b2b5eb2384d617a73
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-05-05 01:21:08 -0500

    Update gen_core.sh

--- sample/gen_core.sh
+++ sample/gen_core.sh
@@ -1,6 +1,7 @@
 mkdir -p core
 for i in `ls haskell`; do
-  ghc -O0 -ddump-simpl haskell/$i | tail -n +5 > core/$(basename $i .hs).hcr
+  ghc -O0 -ddump-simpl haskell/$i | tail -n +5 > core/$(basename $i .hs).hcr;
+  echo "- Writing core/$(basename $i .hs).hcr..."
 done
 rm haskell/*.o
 rm haskell/*.hi
