c9c0bc2a35bd8b2bc5296cd3f88daa81235e93f8
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-05-15 10:06:58 -0500

    Use -ddump-ds

--- sample/gen_core.sh
+++ sample/gen_core.sh
@@ -1,6 +1,6 @@
 mkdir -p core
 for i in `ls haskell`; do
-  ghc -O2 -ddump-simpl -dsuppress-idinfo haskell/$i | tail -n +5 > core/$(basename $i .hs).hcr;
+  ghc -ddump-ds -dsuppress-idinfo haskell/$i | tail -n +6 > core/$(basename $i .hs).hcr;
   echo "- Writing core/$(basename $i .hs).hcr..."
 done
 rm haskell/*.o
