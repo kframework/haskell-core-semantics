ffc532bb9528cade1d15a84bedcee55a9ef76538
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-05-23 13:42:54 -0500

    Run `to-core` with `--no-types`

--- test/gen_core.sh
+++ test/gen_core.sh
@@ -1,6 +1,6 @@
 mkdir -p core
 cd haskell
 for i in `ls *.hs`; do
-  to-core $(basename $i .hs) -o ../core/$(basename $i .hs).pkore;
+  to-core --no-types $@ $(basename $i .hs) -o ../core/$(basename $i .hs).pkore;
   echo "- Writing core/$(basename $i .hs).pkore..."
 done
