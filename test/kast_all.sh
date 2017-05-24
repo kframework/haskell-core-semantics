6f66f3d0f38f8456417d12c9cefd37261bc8b71c
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-05-23 22:09:34 -0500

    Fold output to 80 characters

--- test/kast_all.sh
+++ test/kast_all.sh
@@ -4,6 +4,6 @@ cd ..
 for i in `ls test/core`; do
   echo "RUNNING \`kast test/core/$i\`"
   echo "--------------------------------------------------------------------------------"
-  time kast test/core/$i
+  time kast test/core/$i | fold -w 80
   echo "\n"
 done
