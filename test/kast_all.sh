e9fa2a66d97e7fcaa726ad6d1e790617936a47f3
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-05-23 21:36:56 -0500

    Add script for running kast on all sample programs

---
+++ test/kast_all.sh
@@ -0,0 +1,9 @@
+./gen_core.sh
+echo "\n\n"
+cd ..
+for i in `ls test/core`; do
+  echo "RUNNING \`kast test/core/$i\`"
+  echo "--------------------------------------------------------------------------------"
+  time kast test/core/$i
+  echo "\n"
+done
