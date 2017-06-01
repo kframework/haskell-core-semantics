05e56959825fd96ebe39c6da6126f3b8810bcdc2
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-06-01 09:11:33 -0500

    Add new sample Haskell program that cannot be kast'ed

---
+++ test/haskell/BouncyNumbers.hs
@@ -0,0 +1,22 @@
+module BouncyNumbers where
+
+digits n = reverse $ inverseDigits n
+           where inverseDigits n
+                   | n == 0 = []
+                   | otherwise = (n `mod` 10) : inverseDigits (n `div` 10)
+
+differences (x:[]) = []
+differences (x:xs) = (x - (head xs)) : (differences xs)
+
+bouncy n = any (> 0) diffs && any (< 0) diffs
+           where diffs = differences $ digits n
+
+countBouncyNumbers  = iterateCount 0 0 1
+                      where iterateCount iter count num
+                              | ratio == 0.99 = num - 1
+                              | otherwise = if bouncy num
+                                            then iterateCount (iter + 1) (count + 1) (num + 1)
+                                            else iterateCount (iter + 1) count (num + 1)
+                              where ratio = (fromIntegral count) / (fromIntegral iter)
+
+main = print countBouncyNumbers
