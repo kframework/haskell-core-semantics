2bfcb2635cd8a1e2e56be1b59c840eb35f1dd1d4
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-06-13 10:58:56 -0500

    Move some scripts to `script` dir

---
+++ script/compile_haskell.sh
@@ -0,0 +1,4 @@
+to-core --no-types --strip-result -o out $1
+cat out | grep "^nonRec" > $1.pkore
+echo "" >> $1.pkore
+cat out | grep "^[^nonRec]" >> $1.pkore
---
+++ script/krunhaskell.sh
@@ -0,0 +1,3 @@
+$HASKELL_CORE_SEMANTICS_DIR/script/compile_haskell.sh $1
+echo "krun -d $HASKELL_CORE_SEMANTICS_DIR $1.pkore"
+krun -d $HASKELL_CORE_SEMANTICS_DIR $1.pkore
