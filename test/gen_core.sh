6a262486b73f766845836857031586ddebd35c79
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-06-16 13:50:20 -0500

    Update gen_core.sh

--- test/gen_core.sh
+++ test/gen_core.sh
@@ -1,7 +1,7 @@
 mkdir -p core
 cd haskell
 for i in `ls *.hs`; do
-  echo "file $i"
+  echo "Writing $i..."
   to-core --no-types --strip-result $@ $(basename $i .hs) -o ../core/$(basename $i .hs).pkore;
   # echo "- Writing core/$(basename $i .hs).pkore..."
 done
