07a0707f179adcdee994eba946e3b3ed65567671
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-06-16 16:02:25 -0500

    Fix krunhaskell.sh

--- script/krunhaskell.sh
+++ script/krunhaskell.sh
@@ -1,4 +1,4 @@
-$HASKELL_CORE_SEMANTICS_DIR/script/compile_haskell.sh $1
+to-core --no-types --strip-result -o $1.pkore $1
 echo "krun -d $HASKELL_CORE_SEMANTICS_DIR $1.pkore"
 krun -d $HASKELL_CORE_SEMANTICS_DIR $1.pkore
 rm $1.pkore
